74ed97a5c23a9790a11233b6a320cd698d2c89d9
Ashik Panchkoti <ashikpanchkoti13@gmail.com>
2024-12-15 19:10:45 -0800

    Update files

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-7511649c-9204-4b70-b198-7736da265413.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-27454db3-5923-4ec1-9836-8bcc9df8efa3.containerhub.tripleten-services.com"
 }
